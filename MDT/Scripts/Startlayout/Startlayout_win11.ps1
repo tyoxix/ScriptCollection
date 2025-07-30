0cfc68b00caf2a961b21e9e7330316f725ad51fa
tyoxix <83495834+tyoxix@users.noreply.github.com>
2025-07-30 10:32:19 +0200

    Create Startlayout_win11.ps1

---
+++ MDT/Scripts/Startlayout/Startlayout_win11.ps1
@@ -0,0 +1,2 @@
+xcopy "\\MDTOMI-WS1\DeploymentShare$\Scripts\start.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\" /y
+Import-startlayout -layoutpath "\\MDTOMI-WS1\DeploymentShare$\Scripts\taskbar_win11.xml" -Mountpath "C:\"
